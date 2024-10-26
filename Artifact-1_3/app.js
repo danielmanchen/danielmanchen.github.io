a7425bb63fe777292a064e1d43aee080062668c5
danielmanchen <103309124+danielmanchen@users.noreply.github.com>
2024-10-25 21:22:13 -0500

    Add files via upload

---
+++ Artifact-1_3/app.js
@@ -0,0 +1,69 @@
+var createError = require('http-errors');
+var express = require('express');
+const cors = require('cors');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./app_server/routes/index');
+var usersRouter = require('./app_server/routes/users');
+var apiRouter = require('./app_api/routes/index');
+var handlebars = require('hbs');
+
+require('./app_api/models/db');
+
+var app = express();
+const corsOptions = {
+  origin: 'http://localhost:3000/api/inventory/',
+  methods: 'GET,HEAD,POST,PUT,DELETE',
+  Credentials: true,
+  origin: true
+}
+// view engine setup
+app.set('views', path.join(__dirname, 'app_server', 'views'));
+
+handlebars.registerPartials(__dirname + '/app_server/views/partials');
+
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'front-end/dist/front-end')));
+app.use(express.static(path.join(__dirname, 'public')));
+/* app.options('*', (req, res) => {
+  res.status(200).send("preflight request allowed");
+}); */
+app.use('/api', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200/');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-Width, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+});
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/api', apiRouter);
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+
+module.exports = app;
